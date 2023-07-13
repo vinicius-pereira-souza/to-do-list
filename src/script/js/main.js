a2a78de40a94f6c5ecb55c1018582e9260ecd112
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-07-13 14:25:48 -0300

    feat: criação da função que fara a renderização das notas na tela

--- src/script/js/main.js
+++ src/script/js/main.js
@@ -3,13 +3,35 @@ const container = document.querySelector("main");
 const emptyContainer = document.querySelector(".empty-box");
 const form = document.querySelector("form");
 const input = document.getElementById("textContent");
+let notes = [];
+form.addEventListener("submit", handleSubmitNote);
+function handleSubmitNote(e) {
+    e.preventDefault();
+    if (!input.value) {
+        return;
+    }
+    const note = {
+        text: input.value,
+        check: false,
+        id: Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1),
+    };
+    notes = [...notes, note];
+    localStorage.setItem("notes", JSON.stringify(notes));
+    checkHaveNotes();
+    renderAllNote();
+    input.value = "";
+}
 function checkHaveNotes() {
     const arrNotes = getAllNotes();
     if (arrNotes?.length) {
         emptyContainer.classList.remove("show-box");
+        return true;
     }
     else {
         emptyContainer.classList.add("show-box");
+        return false;
     }
 }
 function getAllNotes() {
@@ -17,4 +39,29 @@ function getAllNotes() {
     const notesArr = JSON.parse(localStorage.getItem("notes"));
     return notesArr;
 }
+function createNoteComponent(text, id) {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    input.setAttribute("type", "checkbox");
+    p.textContent = text;
+    button.textContent = "Delete";
+    div.classList.add("box-note");
+    div.setAttribute("id", id);
+    input.classList.add("input-note");
+    p.classList.add("text-note");
+    button.classList.add("button-note");
+    div.appendChild(input);
+    div.appendChild(p);
+    div.appendChild(button);
+    return div;
+}
+function renderAllNote() {
+    const notes = getAllNotes();
+    [...notes].forEach((note) => {
+        container.appendChild(createNoteComponent(note.text, note.id));
+    });
+}
 checkHaveNotes();
+renderAllNote();
--- src/script/ts/main.ts
+++ src/script/ts/main.ts
@@ -10,14 +10,42 @@ interface Note {
 }
 
 type hasNote = string | null;
+let notes: Note[] = [];
 
-function checkHaveNotes(): void {
+form!.addEventListener("submit", handleSubmitNote);
+
+function handleSubmitNote(e: { preventDefault: () => void }) {
+  e.preventDefault();
+
+  if (!input!.value) {
+    return;
+  }
+
+  const note = {
+    text: input!.value,
+    check: false,
+    id: Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1),
+  };
+
+  notes = [...notes, note];
+  localStorage.setItem("notes", JSON.stringify(notes));
+  checkHaveNotes();
+  renderAllNote();
+
+  input!.value = "";
+}
+
+function checkHaveNotes(): boolean {
   const arrNotes = getAllNotes();
 
   if (arrNotes?.length) {
     emptyContainer!.classList.remove("show-box");
+    return true;
   } else {
     emptyContainer!.classList.add("show-box");
+    return false;
   }
 }
 
@@ -28,4 +56,36 @@ function getAllNotes(): Note[] {
   return notesArr;
 }
 
+function createNoteComponent(text: string, id: string): HTMLDivElement {
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const input = document.createElement("input");
+  const button = document.createElement("button");
+
+  input.setAttribute("type", "checkbox");
+  p.textContent = text;
+  button.textContent = "Delete";
+
+  div.classList.add("box-note");
+  div.setAttribute("id", id);
+  input.classList.add("input-note");
+  p.classList.add("text-note");
+  button.classList.add("button-note");
+
+  div.appendChild(input);
+  div.appendChild(p);
+  div.appendChild(button);
+
+  return div;
+}
+
+function renderAllNote(): void {
+  const notes = getAllNotes();
+
+  [...notes].forEach((note: any) => {
+    container!.appendChild(createNoteComponent(note.text, note.id));
+  });
+}
+
 checkHaveNotes();
+renderAllNote();
